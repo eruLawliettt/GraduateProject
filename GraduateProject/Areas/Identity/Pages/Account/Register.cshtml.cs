229b8bc5426839bfdf4d6f9c2550926dd7c23101
eruLawliet <coffeelancerr@gmail.com>
2023-05-04 20:33:32 +0300

    fuckfuck

--- GraduateProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GraduateProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -130,6 +130,11 @@ namespace GraduateProject.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                user.MiddleName = Input.MiddleName;
+
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
